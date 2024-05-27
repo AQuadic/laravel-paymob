1fa1ae7181efb62db40510473cf0470a396d97a7
Saifallak <Saifallak@yahoo.com>
2024-05-27 15:18:22 +0300

    revert. hmac.

--- src/config/paymob.php
+++ src/config/paymob.php
@@ -10,12 +10,13 @@ return [
         'currency'        => 'EGP',
         'exp_after'       => 10, // seconds
         'min_amount'      => 5, // pounds
+        'hmac'        => env('ACCEPT_HMAC'),
         'url'             => [
             'token'       => 'https://accept.paymob.com/api/auth/tokens',
             'order'       => 'https://accept.paymob.com/api/ecommerce/orders',
             'payment_key' => 'https://accept.paymob.com/api/acceptance/payment_keys',
             'refund'      => 'https://accept.paymob.com/api/acceptance/void_refund/refund',
-            'hmac'        => env('ACCEPT_HMAC'),
+            'hmac'        => 'https://accept.paymobsolutions.com/api/acceptance/transactions',
         ],
         'payment_types' => [
             'card_payment' => [
