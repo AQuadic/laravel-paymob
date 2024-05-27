f29b8da387307d0a9ade996126a4e11f4a85edf6
Saifallak <Saifallak@yahoo.com>
2024-05-27 14:43:07 +0300

    update config to match new api endpoints.

--- src/config/paymob.php
+++ src/config/paymob.php
@@ -11,19 +11,19 @@ return [
         'exp_after'       => 10, // seconds
         'min_amount'      => 5, // pounds
         'url'             => [
-            'token'       => 'https://accept.paymobsolutions.com/api/auth/tokens',
-            'order'       => 'https://accept.paymobsolutions.com/api/ecommerce/orders',
-            'payment_key' => 'https://accept.paymobsolutions.com/api/acceptance/payment_keys',
-            'refund'      => 'https://accept.paymobsolutions.com/api/acceptance/void_refund/refund',
-            'hmac'        => 'https://accept.paymobsolutions.com/api/acceptance/transactions',
+            'token'       => 'https://accept.paymob.com/api/auth/tokens',
+            'order'       => 'https://accept.paymob.com/api/ecommerce/orders',
+            'payment_key' => 'https://accept.paymob.com/api/acceptance/payment_keys',
+            'refund'      => 'https://accept.paymob.com/api/acceptance/void_refund/refund',
+            'hmac'        => env('ACCEPT_HMAC'),
         ],
         'payment_types' => [
             'card_payment' => [
-                'url'            => 'https://accept.paymobsolutions.com/api/acceptance/iframes/' . env('ACCEPT_CARD_IFRAME_ID'),
+                'url'            => 'https://accept.paymob.com/api/acceptance/iframes/' . env('ACCEPT_CARD_IFRAME_ID'),
                 'integration_id' => env('ACCEPT_CARD_INTEGRATION_ID'),
             ],
             'mobile_wallet' => [
-                'url'            => 'https://accept.paymobsolutions.com/api/acceptance/payments/pay',
+                'url'            => 'https://accept.paymob.com/api/acceptance/payments/pay',
                 'integration_id' => env('ACCEPT_MW_INTEGRATION_ID'),
             ],
         ],
